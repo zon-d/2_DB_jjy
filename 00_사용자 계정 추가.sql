07fa5df0f238530434c0008f6d06715b824056e6
zon_d <zon_d@icloud.com>
2022-09-01 09:16:19 +0900

    Initial commit

---
+++ 00_사용자 계정 추가.sql
@@ -0,0 +1,18 @@
+-- 한 줄 주석
+
+/*
+ 범위 주석
+ * */
+
+-- SQL 하나 수행 : CTRL + ENTER
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+
+-- 실습용 사용자 계정 생성
+CREATE USER kh_jjy IDENTIFIED BY kh1234;
+
+-- 사용자 계정 권한 부여
+GRANT RESOURCE, CONNECT TO kh_jjy;
+
+-- 객체 생성(테이블 등) 공간 할당량 지정
+ALTER USER kh_jjy DEFAULT TABLESPACE SYSTEM
+QUOTA UNLIMITED ON SYSTEM;
