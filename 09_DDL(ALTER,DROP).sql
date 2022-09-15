1b677c0355596cf17416c1ec6a16fe48f6bc59c4
zon_d <zon_d@icloud.com>
2022-09-15 17:33:16 +0900

    20220915

--- 09_DDL(ALTER,DROP).sql
+++ 09_DDL(ALTER,DROP).sql
@@ -53,5 +53,188 @@ ALTER TABLE DEPT_COPY
 MODIFY DEPT_TITLE NULL;
 
 
+-------------------------------------------------------------------------------------------
+
+-- 2. 컬럼(추가/수정/삭제)
+
+-- 컬럼 추가
+-- ALTER TABLE 테이블명 ADD(컬럼명 데이터타입 [DEFAULT '값']);
+
+-- 컬럼 수정
+-- ALTER TABLE 테이블명 MODIFY 컬럼명 데이터타입; -> 데이터 타입 변경
+
+-- ALTER TABLE 테이블명 MODIFY 컬럼명 DEFAULT '값'; -> DEFAULT 값 변경
+
+-- ALTER TABLE 테이블명 MODIFY 컬럼명 NULL/NO NULL; -> NULL 여부 변경
+
+-- 컬럼 삭제
+-- ALTER TABLE 테이블명 DROP(삭제할 컬럼명);
+-- ALTER TABLE 테이블명 DROP DROP COLUMN 삭제할 컬럼명;
+
+--> * 컬럼 삭제 시 주의사항! *
+-- 테이블이란? 행과 열로 이루어진 DB에 가장 기본적인 객체
+-- 				테이블에 데이터가 저장됨. 
+
+-- 테이블은 최소 1개 이상의 컬럼이 존재해야 되기 때문에
+-- 모든 컬럼을 다 삭제할 순 없다
+SELECT * FROM DEPT_COPY;
+
+-- CNAME 컬럼 추가
+ALTER TABLE DEPT_COPY ADD(CNAME VARCHAR2(30));
+SELECT * FROM DEPT_COPY;
+
+-- LNAME 컬럼 추가(기본값 '한국')
+ALTER TABLE DEPT_COPY ADD(LNAME VARCHAR2(30) DEFAULT '한국');
+SELECT * FROM DEPT_COPY;
+--> 컬럼이 생성되면서 DEFAULT 값이 자동 삽입됨.
+
+
+-- D10 개발1팀 추가
+INSERT INTO DEPT_COPY
+VALUES('D10', '개발1팀', 'L1', DEFAULT, DEFAULT);
+-- ORA-12899: "KH_JJY"."DEPT_COPY"."DEPT_ID" 열에 대한 값이 너무 큼(실제: 3, 최대값: 2)
+
+-- DEPT_ID 컬럼 (CHAR(2)라서 'D10'이 입력이 안되기때문에)
+ALTER TABLE DEPT_COPY MODIFY DEPT_ID VARCHAR(3);
+--> 컬럼 변경 후 다시 위 INSERT 실행 -> 성공
+
+SELECT * FROM DEPT_COPY;
+
+-- LNAME의 기본값을 'KOREA'로 수정
+ALTER TABLE DEPT_COPY MODIFY LNAME DEFAULT 'KOREA';
+SELECT * FROM DEPT_COPY;
+--> 기본값을 변경했다고 해서 기존 데이터가 변하지는 않음
+
+-- LNAME '한국' -> 'KOREA'로 변경
+UPDATE DEPT_COPY SET
+LNAME = DEFAULT
+WHERE LNAME = '한국';
+
+SELECT * FROM DEPT_COPY; -- UPDATE 확인
+COMMIT;
+
+-- 모든 컬럼 삭제
+ALTER TABLE DEPT_COPY DROP(LNAME);
+ALTER TABLE DEPT_COPY DROP COLUMN CNAME;
+ALTER TABLE DEPT_COPY DROP(LOCATION_ID);
+ALTER TABLE DEPT_COPY DROP(DEPT_TITLE);
+ALTER TABLE DEPT_COPY DROP(DEPT_ID);
+-- ORA-12983: 테이블에 모든 열들을 삭제할 수 없습니다 * 테이블 당 컬럼 1개씩은 있어야해서
+
+
+SELECT * FROM DEPT_COPY;
+
+-- 테이블 삭제
+DROP TABLE DEPT_COPY;
+
+-- DEPARTMENT 테이블 복사해서 DEPT_COPY 생성
+CREATE TABLE DEPT_COPY
+AS SELECT * FROM DEPARTMENT;
+--> 컬럼명, 데이터타입, NOT NULL 여부만 복사됨 *제약조건이 전부 복사되지 않음
+
+-- DEPT_COPY 테이블에 PK 추가(컬럼 : DEPT_ID, 제약조건명 : D_COPY_PK)
+ALTER TABLE DEPT_COPY ADD CONSTRAINT D_COPY_PK PRIMARY KEY(DEPT_ID)
+
+-- 3. 이름 변경 (컬럼, 제약조건, 테이블)
+
+-- 1) 컬럼명 변경(DEPT_TITLE -> DEPT_NAME)
+ALTER TABLE DEPT_COPY RENAME COLUMN DEPT_TITLE TO DEPT_NAME;
+SELECT * FROM DEPT_COPY
+
+-- 2) 제약조건명 변경(D_COPY_PK -> DEPT_COPY_PK)
+ALTER TABLE DEPT_COPY RENAME CONSTRAINT D_COPY_PK TO DEPT_COPY_PK;
+
+-- 3) 테이블명 변경(DEPT_COPY -> DCOPY)
+ALTER TABLE DEPT_COPY RENAME TO DCOPY;
+
+SELECT * FROM DCOPY;
+
+
+
+-------------------------------------------------------------------------------------------
+
+-- 4. 테이블 삭제
+
+-- DROP TABLE 테이블명 [CASCADE CONSTRAINTS]
+
+-- 1) 관계가 형성되지 않은 테이블(DCOPY) 삭제
+DROP TABLE DCOPY;
+
+-- 2) 관계가 형성된 테이블 삭제
+CREATE TABLE TB1(
+	TB1_PK NUMBER PRIMARY KEY,
+	TB1_COL NUMBER
+);
+
+CREATE TABLE TB2(
+	TB2_PK NUMBER PRIMARY KEY,
+	TB2_COL NUMBER REFERENCES TB1
+);
+
+-- TB1에 샘플 데이터 삽입
+INSERT INTO TB1 VALUES(1, 100);
+INSERT INTO TB1 VALUES(2, 200);
+INSERT INTO TB1 VALUES(3, 300);
+COMMIT;
+
+-- TB2에 샘플데이터 삽입
+INSERT INTO TB2 VALUES(11,1);
+INSERT INTO TB2 VALUES(12,2);
+INSERT INTO TB2 VALUES(13,3);
+INSERT INTO TB2 VALUES(14,4); -- 삽입안됨. TB1에 4가 없어서
+
+-- TB1 삭제
+DROP TABLE TB1;
+-- ORA-02449: 외래 키에 의해 참조되는 고유/기본 키가 테이블에 있습니다 * TB2가 TB1을 참조하고있어서
+--> 해결방법
+-- 1) 자식, 부모 테이블 순서로 삭제
+-- 2) ALTER를 이용해서 FK 제약조건 삭제후 TB1 삭제
+-- 3) DROP TABLE 삭제옵선 CASCADE CONSTRAINTS 사용
+	--> CASCADE CONSTRAINTS : 삭제하려는 테이블과 연결된 FK제약 조건을 모두 삭제
+
+DROP TABLE TB1 CASCADE CONSTRAINTS;
+--> 삭제 성공 * 제약조건도 같이 삭제
+
+
+
+-------------------------------------------------------------------------------------------
+
+
+/* DDL 주의 사항 */
+-- 1) DDL은 COMMIT/ROLLBACK이 되지 않는다.
+ --> ALTER, DROP을 신중하게 진행해야함.
+
+-- 2) DDL과 DML 구문을 섞어서 수행하면 안된다
+--> DDL은 수행 시 존재하고 있는 트랜잭션을 모두 DB에 강제 COMMIT 시킴
+--> DDL이 종료된 후 DML 구문을 수행할 수 있도록 권장!
+
+
+SELECT * FROM TB2;
+COMMIT;
+
+INSERT INTO TB2 VALUES(14, 4);
+INSERT INTO TB2 VALUES(15, 5);
+SELECT * FROM TB2;
+
+-- 컬럼명 변경 DDL
+ALTER TABLE TB2 RENAME COLUMN TB2_COL TO TB2_COLCOL
+
+ROLLBACK;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ 11_DCL.sql
@@ -0,0 +1,135 @@
+/*
+	DCL(Data Control Language) : 데이터를 다루기 위한 권한을 다루는 언어
+	
+	- 계정에 db, db 객체에 대한 접근 권한을 부여(GRANT)하고 회수(REVOKE)하는 언어
+	
+	* 권한의 종류
+
+	1) 시스템 권한 : DB접속, 객체 생성 권한
+	
+	CRETAE SESSION   : 데이터베이스 접속 권한
+	CREATE TABLE     : 테이블 생성 권한
+	CREATE VIEW      : 뷰 생성 권한
+	CREATE SEQUENCE  : 시퀀스 생성 권한
+	CREATE PROCEDURE : 함수(프로시져) 생성 권한
+	CREATE USER      : 사용자(계정) 생성 권한
+	DROP USER        : 사용자(계정) 삭제 권한
+	DROP ANY TABLE   : 임의 테이블 삭제 권한
+	
+	
+	2) 객체 권한 : 특정 객체를 조작할 수 있는 권한
+
+	  권한 종류                 설정 객체
+	    SELECT              TABLE, VIEW, SEQUENCE
+	    INSERT              TABLE, VIEW
+	    UPDATE              TABLE, VIEW
+	    DELETE              TABLE, VIEW
+	    ALTER               TABLE, SEQUENCE
+	    REFERENCES          TABLE
+	    INDEX               TABLE
+	    EXECUTE             PROCEDURE
+
+ */
+
+
+
+/* USER - 계정(사용자)
+
+* 관리자 계정 : 데이터베이스의 생성과 관리를 담당하는 계정.
+                모든 권한과 책임을 가지는 계정.
+                ex) sys(최고관리자), system(sys에서 권한 몇개 제외된 관리자)
+
+
+* 사용자 계정 : 데이터베이스에 대하여 질의, 갱신, 보고서 작성 등의
+                작업을 수행할 수 있는 계정으로
+                업무에 필요한 최소한의 권한만을 가지는 것을 원칙으로 한다.
+                ex) KH_jjy계정(각자 이니셜 계정), workbook 등
+*/
+
+
+-- 1. (SYS) 사용자 계정 생성
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+--> 예전 SQL 방식 허용(계정명을 간단히 작성 가능)
+
+-- [작성법]
+-- CREATE USER 사용자명 IDENTIFIED BY 비밀번호;
+CREATE USER jjy_sample IDENTIFIED BY sample1234;
+
+-- 2. 새 연결(접속방법) 추가
+--> ORA-01045 : CREATE SESSION 권한이 없어서 로그인 실패
+				--> 접속 권한
+
+-- 3. (SYS) 접속 권한 부여
+
+-- [권한 부여 작성법]
+-- GRANT 권한, 권한, .... TO 사용자명;
+GRANT CREATE SESSION TO jjy_sample;
+
+
+-- 4. 다시 연결(접속 방법) 추가 -> 성공
+
+-- 5. (sample) 테이블 생성
+CREATE TABLE TB_TEST(
+	PK_COL NUMBER PRIMARY KEY,
+	CONTENT VARCHAR2(100)
+);
+-- ORA-01031: 권한이 불충분합니다
+--> CREATE TABLE : 테이블 생성 권한
+-- + 데이터를 저장할 수 있는 공간(TABLESPACE)
+
+-- 6. (SYS) 테이블 생성 권한 + TABLESAPCE 할당
+GRANT CREATE TABLE TO jjy_sample;
+
+ALTER USER jjy_sample DEFAULT TABLESPACE
+SYSTEM QUOTA UNLIMITED ON SYSTEM; -- 할당
+
+-- 7. (sample) 다시 테이블 생성
+CREATE TABLE TB_TEST(
+	PK_COL NUMBER PRIMARY KEY,
+	CONTENT VARCHAR2(100)
+);
+
+
+--------------------------------------------------------------------------------------------
+
+-- ROLE(역할) : 권한 묶음
+--> 묶어둔 권한(ROLE) 특정 계정에 부여
+---> 해당 계정은 지정된 권한을 이용해서 특정 역할을 할 수 있다.
+
+-- (SYS) sample 계정에 CONNECT, RESOURCE 부여
+GRANT CONNECT, RESOURCE TO jjy_sample;
+-- CONNECT : DB 접속 권한을 묶어둔 ROLE
+-- RESOURCE : DB 사용을 위한 기본 객체 생성 권한을 묶어둔 ROLE
+
+--------------------------------------------------------------------------------------------
+
+-- * 객체 권한 *
+
+-- kh_jjy   /   jjy_sample 사용자 계정끼리 서로 객체 접근 권한 부여
+
+-- 1. (sample) kh_jjy 계정의 EMPLOYEE 테이블 조회
+SELECT * FROM kh_jjy.EMPLOYEE;
+-- ORA-00942: 테이블 또는 뷰가 존재하지 않습니다
+--> 접근 권한이 없어서 조회 불가
+
+-- 2. (kh) jjy_sample 계정에 EMPLOYEE 테이블 조회 권한 부여
+
+-- [객체 권한 부여 방법]
+-- GRANT 객체권한 ON 객체명 TO 사용자명;
+GRANT SELECT ON EMPLOYEE TO jjy_sample;
+
+-- 3. (sample) 다시 kh_jjy.EMPLOYEE 조회
+SELECT * FROM kh_jjy.EMPLOYEE;
+
+-- KH계정 (운영용 DB)
+-- SAMPLE계정(테스트 DB)
+---> 운영용DB의 데이터를 복사
+---> 테스트
+
+
+
+
+
+
+
+
